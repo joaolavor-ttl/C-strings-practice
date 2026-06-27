f7ed75a955f923ee6d646ded3181dc2b7dec9a17
joaolavor-ttl <miguel.lavor@academico.ifpb.edu.br>
2026-06-27 15:58:06 -0300

    changes and updates

---
+++ more_complexy_exercises/question3.c
@@ -0,0 +1,14 @@
+/*Write a C program to read a sentence from the user.
+Calculate and display the number of words that make up
+this sentence. For example, if the sentence entered by
+the user was:'Good luck dear students', the program should 
+display: 4;*/
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(){
+    
+    return 0;
+}
