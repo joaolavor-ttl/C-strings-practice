cbb6cdec13a208101a85248127be576c48303bb5
joaolavor-ttl <miguel.lavor@academico.ifpb.edu.br>
2026-06-29 11:10:29 -0300

    more updates

--- more_complexy_exercises/question3.c
+++ more_complexy_exercises/question3.c
@@ -9,6 +9,24 @@ display: 4;*/
 #include <ctype.h>
 
 int main(){
-char sentence[100];
+    char sentence[100];
+    printf("Enter a sentence: ");
+    fgets(sentence, sizeof(sentence), stdin);
+    sentence[strcspn(sentence, "\n")] = 0; // Remove newline character if present
+
+    int word_count = 0;
+    int in_word = 0; // Flag to track if we are inside a word
+    int size=strlen(sentence);
+
+    for(int i=0; i<size; i++){
+        if(!isspace(sentence[i]) && !in_word){
+            in_word = 1; // We are now inside a word
+            word_count++; // Increment word count
+        } else if(isspace(sentence[i])){
+            in_word = 0; // We are now outside a word
+        }
+    }
+    printf("Number of words: %d\n", word_count);
+
     return 0;
 }
